50d670b0b2bf191a58bddedf2b04d22c86bafbad
JonghwanWon <jh.wwwon@gmail.com>
2022-06-12 12:20:47 +0900

    feat:SampleChart의 Type Selector 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,18 @@
-import {
-  Center,
-  Container,
-  Divider,
-  Flex,
-  HStack,
-  Heading,
-  Icon,
-  Square,
-  Text,
-  VStack,
-  useToken,
-} from '@chakra-ui/react';
+import { Center, Container, Divider, Flex, HStack, Heading, Icon, Square, Text, VStack } from '@chakra-ui/react';
+import { ChartType } from 'chart.js';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import { Chart } from 'react-chartjs-2';
 import { FaBlogger, FaGithub } from 'react-icons/fa';
 import { IoMdMail } from 'react-icons/io';
 
+import ChartIconButton from '~/components/ChartIconButton';
+import { samplingNumbers } from '~/utils/samplingNumbers';
+
 const Page = ({}) => {
-  const [red700, gray200] = useToken('colors', ['red.700', 'gray.200']);
+  const [currentChartType, setCurrentChartType] = useState<ChartType>('line');
+  const chartTypes: ChartType[] = ['bar', 'line'];
 
   return (
     <>
@@ -36,50 +30,51 @@ const Page = ({}) => {
               <Image src="/images/chamomile.png" layout="fill" />
             </Square>
           </VStack>
-          <Center maxW="xl" w="full" m="0 auto" p={4} borderWidth="1px" borderColor="gray.200" rounded="lg">
-            <Chart
-              type="line"
-              options={{
-                responsive: true,
-                aspectRatio: 16 / 9,
-                plugins: {
-                  title: {
-                    display: true,
-                    text: 'Line Chart',
-                    color: red700,
-                    font: { size: 16 },
+          <VStack spacing="2">
+            <HStack spacing="1">
+              {chartTypes.map((type) => (
+                <ChartIconButton
+                  chartType={type}
+                  isActive={type === currentChartType}
+                  onClick={() => setCurrentChartType(type)}
+                />
+              ))}
+            </HStack>
+            <Center maxW="xl" w="full" m="0 auto" p={4} borderWidth="1px" borderColor="gray.200" rounded="lg">
+              <Chart
+                key={currentChartType}
+                type={currentChartType}
+                options={{
+                  responsive: true,
+                  aspectRatio: 16 / 9,
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: `${currentChartType.replace(/^./g, (t) => t.toUpperCase())} Chart`,
+                      font: { size: 16 },
+                    },
                   },
-                },
-              }}
-              data={{
-                labels: ['a', 'b', 'c', 'd', 'e'],
-                datasets: [
-                  {
-                    label: 'Dataset 1',
-                    data: [12, 19, 3, 5, 2, 3],
-                    fill: false,
-                    // lineTension: 0.1,
-                    tension: 0,
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: red700,
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: red700,
-                    pointHoverBorderColor: gray200,
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 3,
-                    pointHitRadius: 15,
-                  },
-                ],
-              }}
-            />
-          </Center>
+                }}
+                data={{
+                  labels: ['a', 'b', 'c', 'd', 'e'],
+                  datasets: [
+                    {
+                      label: 'Dataset 1',
+                      data: samplingNumbers({ count: 5 }),
+                      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                      borderColor: 'rgba(255, 99, 132, 1)',
+                    },
+                    {
+                      label: 'Dataset 2',
+                      data: samplingNumbers({ count: 5 }),
+                      backgroundColor: 'rgba(99, 102, 255, 0.2)',
+                      borderColor: 'rgb(99, 102, 255, 1)',
+                    },
+                  ],
+                }}
+              />
+            </Center>
+          </VStack>
           <Flex as="footer" align="center" justify="space-between" px={[4, 12]}>
             <Text fontSize="sm">All rights reserved. by JonghwanWon 2022</Text>
             <HStack>
